5db8cc73ff46fcb1ee84f1ebf51e2259d766da28
TSCG <tsuchiyahideto@gmail.com>
2019-12-18 21:08:56 +0900

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,14 @@
+#-*- coding:UTF-8 -*-
+
+#*******************************************************************************
+'''
+Tool Name     : Ts_VrayIDlist.__init__
+Description   : VrayID listup(OBJ and Mat)
+'''
+#*******************************************************************************
+
+###### Custom Modules
+import gui
+
+def main():
+	gui.main()
